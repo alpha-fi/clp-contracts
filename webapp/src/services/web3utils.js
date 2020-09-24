827ad289d3b72322dd23380235cbeb76227711f2
Jameson Hodge <jamesondh@gmail.com>
2020-09-24 08:52:57 -0400

    Remove unused functions in web3utils.js

--- webapp/src/services/web3utils.js
+++ webapp/src/services/web3utils.js
@@ -1,28 +1,5 @@
 import { human_standard_token_abi } from "./human_standard_token_abi";
 
-const promisify = (inner) =>
-  new Promise((resolve, reject) =>
-    inner((err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    })
-  );
-
-const getBalance = async (web3, ethAccount) => {
-  let wei, balance;
-  wei = promisify(callback => web3.eth.getBalance(ethAccount, callback));
-  try {
-    balance = await web3.utils.fromWei(await wei, 'ether');
-    return balance;
-  } catch (e) {
-    console.log(`Could not retrieve Ether balance`);
-    console.log(e);
-  }
-}
-
 const getERC20Balance = async (web3, ethAddr, tokenAddr) => {
   
   const tokenContract = await new web3.eth.Contract(human_standard_token_abi, tokenAddr);

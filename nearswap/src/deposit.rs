fb89b37d975438318fc3004ce14f45abfa61565e
amityadav0 <amy29981@gmail.com>
2021-03-23 13:12:58 +0530

    update

--- nearswap/src/deposit.rs
+++ nearswap/src/deposit.rs
@@ -47,14 +47,12 @@ impl NearSwap {
     Deposits attached NEAR.
     Panics if the sender account is not registered. */
     #[payable]
-    pub fn deposit_near(&mut self, registration_only: Option<bool>) {
+    pub fn deposit_near(&mut self) {
         let sender = env::predecessor_account_id();
         let registration_only = registration_only.unwrap_or(false);
         let mut d = self.get_deposit(&sender);
         let amount = env::attached_deposit();
-        if !registration_only {
-            d.near += amount;
-        }
+        d.near += amount;
         self.deposits.insert(&sender, &d);
         env_log!("Deposit, {} yNEAR", amount);
     }
